b8332a04310d14338ffa6657bc51f76aba433b43
Rajneesxx <rajneeshpande.0@gmail.com>
2025-08-11 11:38:12 +0530

    Update CombinedCalculator.tsx

--- src/components/CombinedCalculator.tsx
+++ src/components/CombinedCalculator.tsx
@@ -59,7 +59,7 @@ export const CombinedCalculator = ({
     salaryPerBiller: 50000,
     salaryPerPhysician: 350000,
     avgTimePerPhysicianPerChart: 15,
-    avgTimePerCoderPerChart: 0.1167, // 7 minutes per chart
+    avgTimePerCoderPerChart: 0.1333, // 8 minutes per chart by default
     chartsPerCoderPerDay: 80, // Will be calculated dynamically
     costPerDeniedClaim: 13,
     codingBacklogPercent: 20,
