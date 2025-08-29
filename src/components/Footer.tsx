e43646a34a404cf9b618acdbb1ecba7e024b3966
Rajneesxx <rajneeshpande.0@gmail.com>
2025-08-29 11:36:54 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export const Footer: React.FC = () => {
   };
 
   return (
-   <footer className="bg-[#01101F] w-screen relative overflow-hidden m-0 pt-0 mt-0 left-0 right-0">
+   <footer className="bg-[#01101F] w-screen relative overflow-hidden m-0 pt-0 mt-0 left-0 right-0" style={{ marginLeft: 'calc(-50vw + 50%)', marginRight: 'calc(-50vw + 50%)' }}>
      <div className="max-w-[1240px] mx-auto px-6 md:px-12 pb-16 relative z-10">
 
 
