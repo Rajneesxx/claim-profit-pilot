0d7fba3ab384a70e017cacf7b874bff35ece6e08
Rajneesxx <rajneeshpande.0@gmail.com>
2025-08-29 13:53:14 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,10 @@
 import { ROICalculator } from "@/components/ROICalculator";
-import { SpreadsheetTestButton } from "@/components/SpreadsheetTestButton";
 import { Toaster } from "@/components/ui/toaster";
 
 const Index = () => {
   return (
     <>
       <ROICalculator />
-      <SpreadsheetTestButton />
       <Toaster />
     </>
   );
