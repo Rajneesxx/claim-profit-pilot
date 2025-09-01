a8b468afa1dfd054e06d3f1dc86c42113c54e35d
Rajneesxx <rajneeshpande.0@gmail.com>
2025-09-01 18:13:34 +0530

    Update NavigationHeader.tsx

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -10,7 +10,7 @@ import {
 export const NavigationHeader = () => {
   return (
           <header className="bg-slate-900 fixed top-4 left-1/2 -translate-x-1/2 z-50 rounded-lg shadow-lg">
-        <div className="flex items-center justify-between h-12 px-6 w-auto">
+        <div className="flex items-center justify-between h-14 px-8 w-auto">
 
         {/* Logo */}
         <div className="flex-shrink-0">
